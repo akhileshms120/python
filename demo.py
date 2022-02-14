5c08a163592ba55749de6656d923c648a4bf73af
akhileshms120 <akhileshcommand@gmail.com>
2022-02-14 12:50:06 +0530

    first commit

--- demo.py
+++ demo.py
@@ -1,5 +1,17 @@
-birth_year=input("Birth Year")
+# birth_year=input("Birth Year")
 
-a=int(birth_year)
-age=2022-a
-print(age)
+# a=int(birth_year)
+# age=2022-a
+# print(age)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = age
+    self.age = name
+    # self.age=name
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
