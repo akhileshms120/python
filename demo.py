3b82228837ccfa31f72aece1a1367202a774e924
akhileshms120 <akhileshcommand@gmail.com>
2022-02-15 14:00:35 +0530

    first commit

--- demo.py
+++ demo.py
@@ -5,13 +5,33 @@
 # print(age)
 
 
-class Person:
-  def __init__(self, name, age):
-    self.name = age
-    self.age = name
-    # self.age=name
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = age
+#     self.age = name
+#     # self.age=name
 
-p1 = Person("John", 36)
+# p1 = Person("John", 36)
 
-print(p1.name)
-print(p1.age)
+# print(p1.name)
+# print(p1.age)
+
+
+# Python program to illustrate 
+# function with main
+def getInteger():
+    result = int(input("Enter integer: "))
+    return result
+  
+def Main():
+    print("Started")
+  
+    # calling the getInteger function and 
+    # storing its returned value in the output variable
+    output = getInteger()     
+    print(output)
+  
+# now we are required to tell Python 
+# for 'Main' function existence
+if __name__=="__main__":
+    Main()
